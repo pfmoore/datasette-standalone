967ba12d9c0adee765e311c5d32a746f6170bdb4
Paul Moore <p.f.moore@gmail.com>
2020-10-09 23:06:14 +0100

    datasette 0.50

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import urllib.request
 import zipfile
 
 
-DATASETTE_VERSION = "0.48"
+DATASETTE_VERSION = "0.50"
 
 # Used to release manifest bug fixes without incrementing datasette version.
 MANIFEST_BUILD_NUMBER = 0
